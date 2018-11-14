62e7f77cd591f8a18cb2e4073423a525b37c2ceb
ethantanen <e_tanen@coloradocollege.edu>
2018-11-14 10:17:37 -0700

    supports multiple hidden layers of any size

--- controller.py
+++ controller.py
@@ -2,9 +2,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import scale
+from sklearn.neural_network import MLPClassifier
+import timeit
+import matplotlib.pyplot as plt
+from plot_decision_regions import graph
 
 # machines
-from dnn import DNN
+from fnn import FNN
 
 # helper function to calculate mismatches
 def mismatches(x,y,title):
@@ -17,28 +21,37 @@ def mismatches(x,y,title):
 
 # read in dataset
 df = pd.read_csv('dataset.txt')
-# df = df.sample(frac=1)
+df = df.sample(frac=1)
 
 # seperate feature vectors and labels
 X = df.iloc[:,:-1].values
-print(X[0])
 X = scale(X)
-print(X[0])
 y = df.iloc[:, [4]].values
 
 # y_number for adaline, hot_y is one-hot encoding of y for logisticregression_net
 y_num = [1 if i=='setosa' else 2 if i=='versicolor' else 3 for i in y]
 hot_y = np.delete(np.eye(4)[y_num], 0, axis=1)
 
-print('\n\nMachines with 2 Features for Graphing\n')
-
-d = DNN(shape=[4,2,3], eta =.1, n_epoch=100)
-print(d.shape)
+# run my neural net
+start = timeit.default_timer()
+d = FNN(shape=[4,1,1,3], eta =.1, n_epoch=100)
 d.fit(X, hot_y)
 p = d.predict(X)
 p = [list(i).index(max(list(i))) + 1 for i in p]
+stop = timeit.default_timer()
+# plt.plot(d.Error)
+# plt.show()
+print('\n\nTime: ', stop-start)
+mismatches(p,y_num,'My network:')
 
-mismatches(p,y_num,'ass')
-
+# run sklearns neural net
+start = timeit.default_timer()
+d = MLPClassifier(hidden_layer_sizes=[1,1], learning_rate_init=.1, max_iter=100)
+d.fit(X, hot_y)
+p = d.predict(X)
+p = [list(i).index(max(list(i))) + 1 for i in p]
+stop = timeit.default_timer()
+print('\n\nTime: ', stop-start)
+mismatches(p, y_num, 'Sklearns network:')
 
 print('\n\nEnd.\n')
--- fnn.py
+++ fnn.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pprint import pprint
 from copy import copy
-class DNN ():
+class FNN ():
 
     def __init__(self, shape, eta=1, n_epoch=1000):
 
@@ -10,15 +10,19 @@ class DNN ():
         self.shape = shape
         self.num_layers = len(shape)
         self.initialize(shape)
+        self.Error = []
 
     def fit(self, X, y):
         for _ in range(self.n_epoch):
+            error = []
             for x,y_ in zip(X,y):
                 self.feed_foward(x)
-                self.back_propagate(y_)
+                e = self.back_propagate(y_)
+                error.append(e)
+            self.Error.append(sum(e))
 
     def predict(self, X):
-        return [copy(self.feed_foward(x)) for x in X]
+        return np.array([copy(self.feed_foward(x)) for x in X])
 
     def feed_foward(self, X):
         self.z[0][:-1] = X
@@ -32,16 +36,21 @@ class DNN ():
 
         # helper functions to format data and calculate gradient (delta weights) and error
         gradient = lambda z, e: -self.eta * np.dot(np.atleast_2d(z).T, np.atleast_2d(e))
-        error = lambda e, w, p: np.dot(e, np.atleast_2d(w).T) * p
+        error = lambda e, w, p: np.dot(np.atleast_2d(e), np.atleast_2d(w).T) * p
 
         # calculate error in output layer and update weights
         self.E[-1] = (self.z[-1][:-1] - y) * self.act_prime[-1][:-1]
         self.w[-1] += gradient(self.z[-2], self.E[-1])
 
+        #TODO: fix last line in loop --> ugly and not needed! 
         # propagate error back through net and update weights
         for i in range(2, self.num_layers):
             self.E[-i] = error(self.E[-i+1], self.w[-i+1], self.act_prime[-i])
-            self.w[-i] += gradient(self.z[-i-1], self.E[-i][:-1])
+            self.w[-i] += gradient(self.z[-i-1], np.atleast_2d(self.E[-i][0][:-1]))
+            self.E[-i] = self.E[-i][0][:-1]
+
+        return (self.z[-1][:-1]-y)**2
+
 
     def activation(self, X):
         return 1 / (1+np.exp(-X))
